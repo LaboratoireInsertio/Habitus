2fe88d8d9d328cbfad0373d7602e30b1bf19491d
Jaime Patarroyo <j@jaimepatarroyo.com>
2016-12-30 15:15:41 -0500

    Add mqtt communication with remote Arduino

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var io = require('socket.io')(server);
 var winston = require('winston');
 winston.level = 'debug';
 var serialport = require('serialport'); // include the serialport library
+// include mosca library for mqtt communication
+var mosca = require('mosca')
 
 var pirActive = false,
 		CapturingSoundGlobal = false,
@@ -44,8 +46,18 @@ parser: serialport.parsers.readline('\n')
 // open the serial port:
 var myPort = new SerialPort("/dev/cu.usbserial-DA013UAA", portConfig);
 
-// ------------------------------------------------------------- //
+//---------- MQTT CONFIGURATION SERVER <-> REMOTE ARDUINO ----------//
 
+var mqtt_settings = {
+  port: 1883,
+  persistence: mosca.persistence.Memory
+};
+ 
+var mqtt_server = new mosca.Server(mqtt_settings, function() {
+  //console.log('Mosca server is up and running')
+});
+
+// ------------------------------------------------------------- //
 
 
 // Listen connection Client Side for send datas of sensors from MongoDB
@@ -152,8 +164,8 @@ myPort.on('data', function (data) {
    	},200);
    }
 
+   /*
    // PHOTOCELLS
-
    if(dataSplit[2] >= 500 && !photoCellDownActive ){
    		photoCellDownActive = new Date().getTime();
    		SomeOneInStairs = true;
@@ -185,12 +197,74 @@ myPort.on('data', function (data) {
    		photoCellDownActive = photoCellUpActive = false;
    	},500);
    }
+   */
    
 
 });
 
 
+// Recieve data from remote Arduino
 
+var phoD = 0;
+var phoU = 0;
+var piez = 0;
+
+mqtt_server.published = function(packet, client, cb) {
+  if (packet.topic.indexOf('echo') === 0) {
+    return cb();
+  }
+ 
+  var newPacket = {
+    topic: 'echo/' + packet.topic,
+    payload: packet.payload.toString(),
+    retain: packet.retain,
+    qos: packet.qos
+  };
+
+  if (packet.topic == "feeds/photoDOWN"){
+	phoD = parseInt(packet.payload.toString());
+  } else if (packet.topic == "feeds/photoUP"){
+	phoU = parseInt(packet.payload.toString());
+  } else if (packet.topic == "feeds/piezo"){
+	piez = parseInt(packet.payload.toString());
+  }
+ 
+   // PHOTOCELLS
+   if(phoD >= 500 && !photoCellDownActive ){
+   		photoCellDownActive = new Date().getTime();
+   		SomeOneInStairs = true;
+   		winston.debug('Photocell Down active');
+   }
+
+   if(phoU >= 500 && !photoCellUpActive ){
+   		photoCellUpActive = new Date().getTime();
+   		SomeOneInStairs = true;
+   		winston.debug('Photocell Up active');
+   }
+
+   if(photoCellUpActive && photoCellDownActive && SomeOneInStairs){
+   	SomeOneInStairs =  false;
+
+   	TimeInStairs = photoCellUpActive - photoCellDownActive;
+   	if(TimeInStairs > 0){
+   		winston.debug('Someone up the stairs!', TimeInStairs);
+   		UpOrDown = "up";
+   	}
+   	else {
+   		winston.debug('Someone down the stairs', Math.abs(TimeInStairs));
+   		UpOrDown = "down";
+   	}
+
+    db.stairs.insert({up: photoCellUpActive, down : photoCellDownActive});
+
+   	setTimeout(function(){
+   		photoCellDownActive = photoCellUpActive = false;
+   	},500);
+   }
+  
+  // forward messages to subscribed clients
+  mqtt_server.publish(newPacket, cb);
+}
 
 
 // setInterval(function(){
