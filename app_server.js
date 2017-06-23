a4ccc034494f312d64a019fcd90ecefd6bf4fd26
Chesnel <pa@chesnel.ca>
2017-06-23 12:48:42 -0400

    change log

--- app_server.js
+++ app_server.js
@@ -9,21 +9,20 @@ var io = require('socket.io')(server);
 //-------------- Winston debug -----------------------//
 winston.level = config.debugLevel;
 
-server.listen(8844);
+server.listen(8844, function(){
+	log.info('Server launch on : '+8844);
+});
+// io = io.listen(server);
 
 
+// Listen connection Client Side for send datas of sensors from MongoDB
+io.on('connection', function(socket) {
 
-// Add a connect listener
-io.sockets.on('connection', function(socket) { 
-
-    log.debug('Client connected.');
+    //Get informations abouts sensors
+    log.debug('Connection from client side');
 
     socket.on('status', function(val){
-    	log.debug(val);
-    })
-
-    // Disconnect listener
-    socket.on('disconnect', function() {
-        console.log('Client disconnected.');
+    	log.debug('receive', val);
     });
+
 });
