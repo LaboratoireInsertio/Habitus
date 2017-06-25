1635facc7b24be965fa71037351f940cd32ddba9
Chesnel <pa@chesnel.ca>
2017-06-25 16:05:35 -0400

    create mongo file for manage request database

--- app_server.js
+++ app_server.js
@@ -1,16 +1,17 @@
-const mongojs = require('mongojs');
+// const mongojs = require('mongojs');
 const _ = require('underscore');
 const moment = require('moment');
-const db = mongojs('sensors', ['sound_global', 'sound_loud', 'stairs', 'pir']);
+// const db = mongojs('sensors', ['sound_global', 'sound_loud', 'stairs', 'pir']);
 const config = require('./config.json');
 const express = require('express'),
     winston = log = require('winston');
+
+var db = require('./mongo');
 
 var app = express();
 var server = require('http').Server(app);
 winston.level = config.debugLevel;
 
-
+require('./express').listen(express,server,app,log,db, moment,_);
 require('./sockets').listen(server, log, db, _, moment);
 require('./mosca').listen(server, log, db);
-require('./express').listen(express,server,app,log,db, moment,_);
---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongojs = require('mongojs');
+const _ = require('underscore');
+const moment = require('moment');
+const db = mongojs('sensors', ['sound_global', 'sound_loud', 'stairs', 'pir']);
+
+// Return all data of the DB for the sensors PIR in the entry
+module.exports.getPir = function(cb) {
+  db.pir.find(function(err, docs) {
+    var datas = _.groupBy(docs, function(doc) {
+      return moment(doc.x).startOf('minute').format();
+    });
+    cb(datas);
+  });
+}
+// Return all data of the DB for the sensors loud sound sensor in the office
+module.exports.getSoundLoud = function(cb) {
+  db.sound_loud.find(function(err, docs) {
+    var datas = _.groupBy(docs, function(doc) {
+      return moment(doc.x).startOf('minute').format();
+    });
+    cb(datas)
+  });
+}
+// Return all data of the DB for the sensors photoCell in the stairs
+module.exports.getStairs = function(cb) {
+  db.stairs.find(function(err, docs) {
+    var datas = _.groupBy(docs, function(doc) {
+      return moment(doc.down).startOf('minute').format();
+    });
+    cb(datas)
+  });
+}
+// Return all data of the DB for the global sound sensors ine the office
+module.exports.getSoundGlobal = function(cb) {
+  db.sound_global.find(function(err, docs) {
+    var datas = _.groupBy(docs, function(doc) {
+      return moment(doc.x).startOf('minute').format();
+    });
+    cb(datas)
+  });
+}
