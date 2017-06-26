211ee2191a2402c0a694d7e7eb65ae91c5b8c59c
Jaime Patarroyo <j@jaimepatarroyo.com>
2017-06-26 18:04:41 -0400

    Set Brightness Test

--- app_rasp.js
+++ app_rasp.js
@@ -43,7 +43,7 @@ socket.on('turnAllTintOff', turnAllTintOff)
 // Main Loop
 setTimeout(function() {
   console.log('------------------- START ------------------------');
-  turnAllBulbOn(30); 
+  turnAllBulbOn(80); 
   setInterval(function() {
     //swingBulbUp(200);
     //swingBulbDown(1000);
