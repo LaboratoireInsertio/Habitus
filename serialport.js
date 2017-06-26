4a16577c3228be5d2c56b9595bb5f60f46ff1fd6
Chesnel <pa@chesnel.ca>
2017-06-26 15:00:29 -0400

    try fix bug com arduino serial

--- serialport.js
+++ serialport.js
@@ -4,53 +4,47 @@ const config = require('./config.json');
 
 winston.level = config.debugLevel;
 
-var portArduino = '';
-var serial;
+// var portArduino = '';
+// SerialPort.list(function (err, ports) {
+//   ports.forEach(function(port) {
+//     if(port.manufacturer && port.manufacturer.indexOf('Arduino') <= 0){
+//       log.debug('Arduino detected  : '+port.comName);
+//       portArduino  = port.comName;
+//     }
+//   });
+// });
+// return;
 
-SerialPort.list(function (err, ports) {
-  ports.forEach(function(port) {
-    if(port.manufacturer && port.manufacturer.indexOf('Arduino') <= 0){
-      log.debug('Arduino detected  : '+port.comName);
-      portArduino  = port.comName;
-
-      serial = new SerialPort(portArduino, {
-      // var serial = new SerialPort('/dev/cu.usbmodemfa131', {
-        parser: SerialPort.parsers.readline("\n"),
-        baudRate: 9600
-      });
-
-      serial.on('open', () => {
-        console.log('Port is open!');
-      });
-
-      serial.on('data', (data) => {
-        // if(data.indexOf('~').length <= 0)
-          console.log(data);
-      });
-
-      serial.on('close', () => {
-        console.log('Serial port disconnected.');
-        io.sockets.emit('close');
-      });
-
-
-
-    }
-  });
-});
-
-module.exports.sendToMega = function(type, id, value, cb) {
-  log.debug("send  "+type+ " "+ id + " "+value);
-  serial.write(type + id + value + "~");
-}
 
 ///dev/cu.usbserial-7QVCOHC
-
+// var serial = new SerialPort('/dev/ttyACM0', {
+var serial = new SerialPort('/dev/cu.usbmodemfa131', {
+  parser: SerialPort.parsers.readline("\n"),
+  baudRate: 9600
+});
 
 // Values to send over to Arduino.
 // const MESSAGE = Buffer.from([R5<~]);
 // const LOW = Buffer.from([0]);
 
+serial.on('open', () => {
+  console.log('Port is open!');
+});
+
+serial.on('data', (data) => {
+  // if(data.indexOf('~').length <= 0)
+    // console.log(data);
+});
+
+module.exports.sendToMega = function(type, id, value, cb) {
+  log.debug("send arduino : ",type, id, value);
+  serial.write(type + id + value + "~");
+}
+
+serial.on('close', () => {
+  console.log('Serial port disconnected.');
+  io.sockets.emit('close');
+});
 
 //test working
 // setInterval(function(){
