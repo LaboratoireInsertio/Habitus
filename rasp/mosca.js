485609a01d992c54836d9a355756aad43ff8599d
Chesnel <pa@chesnel.ca>
2017-06-26 15:18:34 -0400

    test mqtt

---
+++ rasp/mosca.js
@@ -0,0 +1,75 @@
+var mosca = require('mosca');
+
+var mqtt_settings = {
+  port: 1883,
+  persistence: mosca.persistence.Memory
+};
+
+var mqtt_server = new mosca.Server(mqtt_settings, function() {
+  console.log('Mosca server is up and running');
+});
+
+
+// Recieve data from remote Arduino
+
+var phoD = 0;
+var phoU = 0;
+var piez = 0;
+
+mqtt_server.published = function(packet, client, cb) {
+  if (packet.topic.indexOf('echo') === 0) {
+    return cb();
+  }
+  console.log(packet);
+
+  var newPacket = {
+    topic: 'echo/' + packet.topic,
+    payload: packet.payload.toString(),
+    retain: packet.retain,
+    qos: packet.qos
+  };
+
+  if (packet.topic == "feeds/photoDOWN"){
+	phoD = parseInt(packet.payload.toString());
+  } else if (packet.topic == "feeds/photoUP"){
+	phoU = parseInt(packet.payload.toString());
+  } else if (packet.topic == "feeds/piezo"){
+	piez = parseInt(packet.payload.toString());
+  }
+
+   // PHOTOCELLS
+   if(phoD >= 500 && !photoCellDownActive ){
+   		photoCellDownActive = new Date().getTime();
+   		SomeOneInStairs = true;
+   		winston.debug('Photocell Down active');
+   }
+
+   if(phoU >= 500 && !photoCellUpActive ){
+   		photoCellUpActive = new Date().getTime();
+   		SomeOneInStairs = true;
+   		winston.debug('Photocell Up active');
+   }
+
+   if(photoCellUpActive && photoCellDownActive && SomeOneInStairs){
+   	SomeOneInStairs =  false;
+
+   	TimeInStairs = photoCellUpActive - photoCellDownActive;
+   	if(TimeInStairs > 0){
+   		winston.debug('Someone up the stairs!', TimeInStairs);
+   		UpOrDown = "up";
+   	}
+   	else {
+   		winston.debug('Someone down the stairs', Math.abs(TimeInStairs));
+   		UpOrDown = "down";
+   	}
+
+    db.stairs.insert({up: photoCellUpActive, down : photoCellDownActive});
+
+   	setTimeout(function(){
+   		photoCellDownActive = photoCellUpActive = false;
+   	},500);
+   }
+
+  // forward messages to subscribed clients
+  mqtt_server.publish(newPacket, cb);
+}
