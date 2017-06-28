6f64b8a5a725af5343a47e03807d480dbb144577
Jaime Patarroyo <j@jaimepatarroyo.com>
2017-06-28 17:39:19 -0400

    Public available functions update

--- only_change_this_file.js
+++ only_change_this_file.js
@@ -9,12 +9,62 @@
  sensors.loudSound		0-1
  sensors.globalSound		0-1024
  sensors.globalActivity	0-200
- */
+*/
 
 // ---- ANIMATIONS -----//
+/*
+ // Turns all the bulbs on, to the given bightness.
+ animations.turnAllBulbOn(brightness)
+
+ // Turns all the bulbs off.
+ animations.turnAllBulbOff()
+
+ // Turns all the tints off.
+ animations.turnAllTintOff()
+
+ // Turns all the tints on.
+ animations.turnAllTintOn()
+
+ // Turns on the bulbs one by one at a given interval and to a given brightness
+ // starting from the bottom one and cycling while the function is called.
+ animations.swingBulbUp(interval, brightness)
+
+ // Turns on the bulbs one by one at a given interval and to a given brightness
+ // starting from the top one and cycling while the function is called.
+ animations.swingBulbDown(interval, brightness)
+
+ // Turns on the tints one by one at a given interval and to a given brightness
+ // starting from the bottom one and cycling while the function is called.
+ animations.swingTintUp(interval)
+
+ // Turns on the tints one by one at a given interval and to a given brightness
+ // starting from the top one and cycling while the function is called.
+ animations.swingTintDown(interval)
+
+ // Turns on the bulbs one by one at a given interval and to a given brightness
+ // in a random order while the function is called.
+ animations.randomBulb(interval, maxBrightness)
+
+ // Turns on the tints one by one at a given interval and to a given brightness
+ // in a random order while the function is called.
+ animations.randomTint(interval)
+
+ // Turns all the the bulbs on, each one with a diferent brightness, slowly
+ // fading between states and taking into account the given  bightness maximum 
+ // and the given interval.
+ animations.randomBulbBrightnessAll(interval, maxBrightness)
+
+ // Randomly toggles all the tints on/off.
+ animations.randomTintToggleAll(interval)
+*/
 
 
 // ---- LAMPS LIFX -----//
+/*
+ lampsLifx.dinningTable
+ lampsLifx.florLamp
+ lampsLifx.kitchen
+*/
 
 
 var lastPir = 0;
