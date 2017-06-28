a151a7abf3209c1ec49996c9af8ee579eb2627b2
Jaime Patarroyo <j@jaimepatarroyo.com>
2017-06-28 08:18:33 -0400

    Added function comments to animations.js

--- animations.js
+++ animations.js
@@ -1,3 +1,13 @@
+/*
+ *
+ *ANIMATIONS.JS
+ *
+ *In this file there are diferent light and tint animations that should
+ *be called from the INTERACTIONS.JS file, inside the main loop. 
+ *They need to be inside a loop to work.
+ *
+ */
+
 var serialport, log;
 var whichBulb1 = 0;
 var timerBulb1 = Date.now();
@@ -38,27 +48,33 @@ module.exports = {
     serialport = sp;
     log.info('Module Animations is initialized');
   },
+  // ------------------ Basic Animations ------------------ //
+  // Turns all the bulbs on, to the given bightness.
   turnAllBulbOn: function(brightness) {
     for (var i = 0; i < numLamps; i++) {
       serialport.sendToMega("D", i + 1, String.fromCharCode(brightness));
     }
   },
-
+  // Turns all the bulbs off.
   turnAllBulbOff: function() {
     for (var i = 0; i < numLamps; i++) {
       serialport.sendToMega("D", i + 1, String.fromCharCode(bulbMin));
     }
   },
+  // Turns all the tints off
   turnAllTintOff: function() {
     for (var i = 0; i < numLamps; i++) {
       serialport.sendToMega("R", i + 1, String.fromCharCode(0));
     }
   },
+  // Turns all the tints on
   turnAllTintOn: function() {
     for (var i = 0; i < numLamps; i++) {
       serialport.sendToMega("R", i + 1, String.fromCharCode(1));
     }
   },
+  // Turns on the bulbs one by one at a given interval and to a given brightness
+  // starting from the bottom one and cycling while the function is called
   swingBulbUp: function(interval, brightness) {
     if ((Date.now() - timerBulb1) >= interval) {
       //console.log("UP! "+bulbMin);
@@ -73,6 +89,8 @@ module.exports = {
       timerBulb1 = Date.now();
     }
   },
+  // Turns on the bulbs one by one at a given interval and to a given brightness
+  // starting from the top one and cycling while the function is called
   swingBulbDown: function(interval, brightness) {
     if ((Date.now() - timerBulb2) >= interval) {
       //console.log("DOWN! "+bulbMax);
@@ -87,6 +105,8 @@ module.exports = {
       timerBulb2 = Date.now();
     }
   },
+  // Turns on the tints one by one at a given interval and to a given brightness
+  // starting from the bottom one and cycling while the function is called
   swingTintUp: function(interval) {
     if ((Date.now() - timerTint1) >= interval) {
       //console.log("UP! "+bulbMin);
@@ -101,6 +121,8 @@ module.exports = {
       timerTint1 = Date.now();
     }
   },
+  // Turns on the tints one by one at a given interval and to a given brightness
+  // starting from the top one and cycling while the function is called
   swingTintDown: function(interval) {
     if ((Date.now() - timerTint2) >= interval) {
       //console.log("UP! "+bulbMin);
@@ -115,6 +137,8 @@ module.exports = {
       timerTint2 = Date.now();
     }
   },
+  // Turns on the bulbs one by one at a given interval and to a given brightness
+  // in a random order while the function is called
   randomBulb: function(interval, maxBrightness) {
     if ((Date.now() - timerRandomBulb) >= interval) {
       //console.log(whichRandomBulb);
@@ -129,6 +153,8 @@ module.exports = {
       timerRandomBulb = Date.now();
     }
   },
+  // Turns on the tints one by one at a given interval and to a given brightness
+  // in a random order while the function is called
   randomTint: function(interval) {
     if ((Date.now() - timerRandomTint) >= interval) {
       //console.log("UP! "+bulbMin);
@@ -142,6 +168,9 @@ module.exports = {
       timerRandomTint = Date.now();
     }
   },
+  // OLD RANDOM BULB BRIGHTNESS Turns all the the bulbs on each one with a
+  // diferent brightness taking into account the given maximum and changing
+  // at the given interval
   //randomBulbBrightnessAll: function(interval, maxBrightness) {
   //  // log.debug('active random Bulb Rightness All');
   //  if ((Date.now() - timerRandomBrightnessAll) >= interval) {
@@ -152,17 +181,11 @@ module.exports = {
   //    timerRandomBrightnessAll = Date.now();
   //  }
   //}	,
-  randomBulbBrightnessAll: function(interval, maxBrightness) {
-	// OLD RANDOM BULB
-    // log.debug('active random Bulb Rightness All');
-    //if ((Date.now() - timerRandomBrightnessAll) >= interval) {
-    //  for (var i = 1; i <= 8; i++) {
-    //    var randomBright = getRandomInt(maxBrightness, bulbMin);
-    //    serialport.sendToMega("D", i, String.fromCharCode(randomBright));
-    //  }
-    //  timerRandomBrightnessAll = Date.now();
-    //}
 
+  // Turns all the the bulbs on, each one with a diferent brightness, slowly
+  // fading between states and taking into account the given  bightness maximum 
+  // and the given interval
+  randomBulbBrightnessAll: function(interval, maxBrightness) {
 	if ((Date.now() - timerRandomBrightnessAll) >= interval) {
 		for (var i = 0; i < 8; i++) {
 			desiredBright[i] = getRandomInt(maxBrightness, bulbMin);
@@ -198,6 +221,7 @@ module.exports = {
 	}
 
   }	,
+  // Randomly toggles all the tints on/off
   randomTintToggleAll: function(interval) {
     // log.debug('active random Bulb Rightness All');
     if ((Date.now() - timerRandomTintToggleAll) >= interval) {
