f2886a325e0edcb921be614a2fe43695ce7a02a9
Jaime Patarroyo <j@jaimepatarroyo.com>
2017-06-28 08:25:02 -0400

    LIGHTS ON interactions.js

--- interactions.js
+++ interactions.js
@@ -154,7 +154,7 @@ function init(sensors, lamps, animations, log, serialport, socket) {
 	
 	// ------------------- Run Main Animation ------------------- //
 	if (!doingSecondaryAnimation1 && !doingSecondaryAnimation2){
-		///////////////////animations.randomBulbBrightnessAll(mainInterval, mainBrightness);
+		animations.randomBulbBrightnessAll(mainInterval, mainBrightness);
 	}
 	
 	
@@ -177,9 +177,9 @@ function init(sensors, lamps, animations, log, serialport, socket) {
 		if (whichBulbSwingUpOnce <= 8){
 			if ((Date.now() - timerBulbSwingUpOnce) >= 500){
 				//log.debug(whichBulbSwingUpOnce);
-				/////////////serialport.sendToMega("D", whichBulbSwingUpOnce, 0);
+				serialport.sendToMega("D", whichBulbSwingUpOnce, 0);
         		whichBulbSwingUpOnce++;
-				/////////////serialport.sendToMega("D", whichBulbSwingUpOnce, mainBrightness);
+				serialport.sendToMega("D", whichBulbSwingUpOnce, mainBrightness);
 
 				timerBulbSwingUpOnce = Date.now();
 			}
@@ -207,9 +207,9 @@ function init(sensors, lamps, animations, log, serialport, socket) {
 		if (whichBulbSwingDownOnce >= 1){
 			if ((Date.now() - timerBulbSwingDownOnce) >= 500){
 				//log.debug(whichBulbSwingDownOnce);
-				/////////////////serialport.sendToMega("D", whichBulbSwingDownOnce, 0);
+				serialport.sendToMega("D", whichBulbSwingDownOnce, 0);
         		whichBulbSwingDownOnce--;
-				/////////////////serialport.sendToMega("D", whichBulbSwingDownOnce, mainBrightness);
+				serialport.sendToMega("D", whichBulbSwingDownOnce, mainBrightness);
 
 				timerBulbSwingDownOnce = Date.now();
 			}
