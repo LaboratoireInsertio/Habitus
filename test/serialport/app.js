7f939083770911fb336e850778c459ef48de97aa
PIerre-Antoine Chesnel <pierre-antoinechesnel@MacBook-Pro-de-PIerre-Antoine.local>
2018-06-20 16:35:49 -0400

    add test for communection serialport

---
+++ test/serialport/app.js
@@ -0,0 +1,23 @@
+var SerialPort = require('serialport');
+var Readline = SerialPort.parsers.Readline;
+var arduinoPort = '/dev/ttyACM0';
+// var arduinoPort = '/dev/ttyACM0';
+var serial = new SerialPort(arduinoPort, {
+  // parser: SerialPort.parsers.readline("\n"),
+  baudRate: 57600
+});
+var parser = serial.pipe(new Readline());
+
+
+serial.on('open', () => {
+  console.log('Arduino port ' + arduinoPort + ' is open');
+});
+
+serial.on('close', () => {
+  console.log('Serialport for arduino is disconnected.');
+});
+
+parser.on('data', (data) => {
+  var dataSplit = data.split(":");
+  console.log(dataSplit);
+});
