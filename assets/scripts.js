34f21f27f657097295148dfeb9f333ee88d6de3b
PIerre-Antoine Chesnel <pierre-antoinechesnel@MacBook-Pro-de-PIerre-Antoine.local>
2018-06-21 11:51:36 -0400

    working on visual interface sensors

--- assets/scripts.js
+++ assets/scripts.js
@@ -33,6 +33,9 @@ socket.on('globalActivity', function(globalActivity) {
 
 socket.on('stateStairs', function(stateStairs) {
   console.log(stateStairs);
+  for(var i=0;i<stateStairs.tints.length;i++){
+    $("data-idtint="+i+"").css("backgroundColor", "rgba(0,0,0,"+stateStairs.tints[i]+")")
+  }
 });
 
 /// UPDATE! : Maintenanant nous utilisons Metabase pour visualiser les données de mongo!
